c43fc6e46dd9cacdf336f458ee2014497488f480
yan9woojin <yan9woojin@gmail.com>
2024-02-15 13:01:30 +0900

    feat: Header 추가

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/components/Header';
+
 export default function RootLayout() {
   return (
     <div>
+      <Header />
       <main>
         <Outlet />
       </main>
